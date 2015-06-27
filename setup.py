9e62185e1de83bde9a10df8ae5fda3e5ee2e241f
Andrew Martinez <andrew.p.martinez@gmail.com>
2015-06-27 17:11:51 -0400

    PyPI settings

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'py-eve-chat-mon',
+  packages = ['py-eve-chat-mon'],
+  version = '0.1',
+  description = 'A python library that monitors 1-n Eve chat logs and publishes events when specific message types are detected. Message types include regular-expression matches and system intel reports.',
+  author = 'Andrew Martinez',
+  author_email = 'andrew.p.martinez@gmail.com',
+  url = 'https://github.com/andrewpmartinez/py-eve-chat-mon',
+  download_url = 'https://github.com/andrewpmartinez/py-eve-chat-mon/tarball/0.1',
+  keywords = ['EVE', 'chat', 'monitor'],
+  classifiers = [],
+)

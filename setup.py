6d82dcdf7881656e9791639fc995a315e802bbb2
Andrew Martinez <andrew.p.martinez@gmail.com>
2015-07-02 23:52:38 -0400

    Include requirements.txt in package

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     url='https://github.com/andrewpmartinez/py-eve-chat-mon',
     download_url='https://github.com/andrewpmartinez/py-eve-chat-mon/tarball/0.2',
     packages=find_packages(),
+    package_data={'': ['requirements.txt']},
     long_description="See github page for full details.",
     classifiers=[
         'Development Status :: 5 - Production/Stable'
